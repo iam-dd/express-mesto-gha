7eb0a702210e1344482ceb25ef56937bdc132b0e
Dmitriy Demchenko <iam@ddemchenko.ru>
2023-03-30 19:53:07 +0300

    created models

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const { PORT = 3000} = process.env
+const mongoose = require('mongoose')
+
+app.listen(PORT, () => {
+  mongoose.connect('mongodb://localhost:27017/mestodb')
+  console.log(`Express started on port ${PORT}`)
+})
