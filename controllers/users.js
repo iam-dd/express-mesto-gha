ef1ecc830dd45ab7d31cb9851ccc7fc14bc12ea5
Dmitriy Demchenko <iam@ddemchenko.ru>
2023-04-06 11:33:55 +0300

    fix error Validation error

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,12 @@
 const User = require('../models/user');
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(err);
-      } else {
-        next(err);
+        res.status(400).send({ message: 'Ошибка валидации' });
       }
     });
 };
@@ -25,11 +23,11 @@ module.exports.getUserById = (req, res) => {
     .catch((err) => res.status(404).send(err.message));
 };
 
-module.exports.updateUser = (req, res, next) => {
+module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(400).send({ message: 'Не корректные данные' }))
+    .catch(() => res.status(400).send({ message: 'Не корректные данные' }));
 };
 
 module.exports.updateAvatar = (req, res, next) => {
